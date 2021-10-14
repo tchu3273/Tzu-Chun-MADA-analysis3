240a96691b53399622dfd5489126cfdb3183f940
Tzu-Chun Chu <vivianchu1109@gmail.com>
2021-10-13 20:02:05 -0400

    Data wrangling, exploratory, and statistical analysis

--- code/analysis_code/analysisscript.R
+++ code/analysis_code/analysisscript.R
@@ -1,69 +1,112 @@
-###############################
-# analysis script
-#
-#this script loads the processed, cleaned data, does a simple analysis
-#and saves the results to the results folder
-
-#load needed packages. make sure they are installed.
-library(ggplot2) #for plotting
-library(broom) #for cleaning up output from lm()
-library(here) #for data loading/saving
-
-#path to data
-#note the use of the here() package and not absolute paths
-data_location <- here::here("data","processed_data","processeddata.rds")
-
-#load data. 
-mydata <- readRDS(data_location)
-
-######################################
-#Data exploration/description
-######################################
-#I'm using basic R commands here.
-#Lots of good packages exist to do more.
-#For instance check out the tableone or skimr packages
-
-#summarize data 
-mysummary = summary(mydata)
-
-#look at summary
-print(mysummary)
-
-#do the same, but with a bit of trickery to get things into the 
-#shape of a data frame (for easier saving/showing in manuscript)
-summary_df = data.frame(do.call(cbind, lapply(mydata, summary)))
-
-#save data frame table to file for later use in manuscript
-summarytable_file = here("results", "summarytable.rds")
-saveRDS(summary_df, file = summarytable_file)
+---
+title: "Model fitting"
+author: "TChu"
+date: "10/13/2021"
+output: html_document
+---
+	
+# Load needed packages
+library(tidymodels) 
+library(tidyverse)
+library(magrittr)
+library(rlang)
+library(dotwhisker)  # for visualizing regression results
 
 
-#make a scatterplot of data
-#we also add a linear regression line to it
-p1 <- mydata %>% ggplot(aes(x=Height, y=Weight)) + geom_point() + geom_smooth(method='lm')
 
-#look at figure
-plot(p1)
+# path to data
+processdata <- readRDS(here::here("data","processed_data","processeddata.rds"))
 
-#save figure
-figure_file = here("results","resultfigure.png")
-ggsave(filename = figure_file, plot=p1) 
+###################
+## Model fitting ##
+###################
 
-######################################
-#Data fitting/statistical analysis
-######################################
+### Linear Linear Model ###
 
-# fit linear model
-lmfit <- lm(Weight ~ Height, mydata)  
+#First, let's set engine for performing linear regression
+linear_fit <- 
+	linear_reg() %>% 
+	set_engine("lm")
 
-# place results from fit into a data frame with the tidy function
-lmtable <- broom::tidy(lmfit)
+##Fits a linear model to the continuous outcome using only the main predictor of interest
+#Linear Model 1: regression body temperature on runny nose 
+lm_1 <- 
+	linear_fit %>% 
+	fit(BodyTemp ~ RunnyNose, data = processdata)
+lm_1
 
-#look at fit results
-print(lmtable)
+#summarize the results
+tidy(lm_1)
 
-# save fit results table  
-table_file = here("results", "resulttable.rds")
-saveRDS(lmtable, file = table_file)
 
-  
+#Linear Model 2: regression body temperature on all predictors
+lm_2 <- linear_fit %>% 
+	fit(BodyTemp ~ ., data = processdata)
+lm_2
+
+#summarize the results
+tidy(lm_2)
+
+#generate a dot-and-whisker plot of linear regression results with all predictors
+dotwhisker_cont_all_pred <- tidy(lm_2) %>% 
+	dotwhisker::dwplot(dot_args = list(size = 2, color = "black"),
+										 whisker_args = list(color = "black"),
+										 vline = geom_vline(xintercept = 0, colour = "grey50", linetype = 2))
+
+#Compares the model results for the linear regression model with just the main predictor and all predictors.
+##Analysis of deviance and model selection
+res_lm_aov <-
+	anova(lm_1$fit, lm_2$fit, test = "Chisq")
+res_lm_aov
+
+#The p-value is smaller than 0.05, therefore the model with all predictors had better fit than the 
+#single variable model
+
+
+### Logistic regression Model ###
+# Let's set engine for performing linear regression
+logistic_fit <- 
+	logistic_reg() %>% 
+	set_mode("classification") %>% 
+	set_engine("glm")
+
+#Logistic Model 1: regression body temperature on runny nose 
+glm_1 <- 
+	logistic_fit %>% 
+	fit(Nausea ~ RunnyNose, data = processdata)
+glm_1
+
+#summarize the results
+tidy(glm_1)
+
+
+#Logistic Model 2: regression body temperature on all predictors
+glm_2 <- logistic_fit %>% 
+	fit(Nausea ~ ., data = processdata)
+glm_2
+
+#summarize the results
+tidy(glm_2)
+
+
+#generate a dot-and-whisker plot of logistic regression results with all predictors
+dotwhisker_binary_all_pred <- tidy(glm_2) %>% 
+	dotwhisker::dwplot(dot_args = list(size = 2, color = "black"),
+										 whisker_args = list(color = "black"),
+										 vline = geom_vline(xintercept = 0, colour = "grey50", linetype = 2))
+
+#Compares the model results for the logistic regression model with just the main predictor and all predictors.
+##Analysis of deviance and model selection
+res_glm_aov <-
+	anova(glm_1$fit, glm_2$fit, test = "Chisq")
+res_glm_aov
+
+#The p-value is smaller than 0.05, therefore the model with all predictors had better fit than the 
+#single variable model
+
+
+# save result tables and plots
+saveRDS(res_lm_aov, file = here::here("results", "res_lm_aov.Rds"))
+saveRDS(res_glm_aov, file = here::here("results", "res_glm_aov.Rds"))
+ggsave(dotwhisker_cont_all_pred, filename = here::here("results", "dotwhisker_cont_all_pred.png"), height = 8.5, width = 11)
+ggsave(dotwhisker_binary_all_pred, filename = here::here("results", "dotwhisker_binary_all_pred.png"), height = 8.5, width = 11)
